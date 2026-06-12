87e69b60d0135be60cf49ffd04d01e0b05adcfb6
Joseph Yu <joseph@josefyu.com>
2026-06-12 10:57:31 +0800

    Move Django DRF folder

--- backend/django-drf/ecommerce/settings.py
+++ backend/django-drf/ecommerce/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "import_export",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -115,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "./static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
